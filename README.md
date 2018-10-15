dc46427afc109099b67d992267fab19c37d15425
Hiroshi Nakahara <nakahara@am.ics.keio.ac.jp>
2018-10-15 23:38:31 +0900

    Add some dependencies; Implement a function to fetch html

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+gradle jar
+java -jar build/libs/cook_crawler-1.0-SNAPSHOT.jar --storagePath PATH_TO_STORAGE
+```
--- src/main/java/jp/hiro116s/cook/crawler/CrawlerTool.java
+++ src/main/java/jp/hiro116s/cook/crawler/CrawlerTool.java
@@ -1,5 +1,75 @@
 package jp.hiro116s.cook.crawler;
 
-public class CrawlerTool {
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.slf4j.Logger;
 
+import java.util.regex.Pattern;
+
+public class CrawlerTool {
+    private static final Pattern PATTERN = Pattern.compile("\\b(((ht|f)tp(s?)\\:\\/\\/|~\\/|\\/)|www.)" +
+            "(\\w+:\\w+@)?(([-\\w]+\\.)+(com|org|net|gov" +
+            "|mil|biz|info|mobi|name|aero|jobs|museum" +
+            "|travel|[a-z]{2}))(:[\\d]{1,5})?" +
+            "(((\\/([-\\w~!$+|.,=]|%[a-f\\d]{2})+)+|\\/)+|\\?|#)?" +
+            "((\\?([-\\w~!$+|.,*:]|%[a-f\\d{2}])+=?" +
+            "([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)" +
+            "(&(?:[-\\w~!$+|.,*:]|%[a-f\\d{2}])+=?" +
+            "([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)*)*" +
+            "(#([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)?\\b");
+
+    public static void main(final String[] args) throws Exception {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel","INFO");
+        final Arguments arguments = parseArgs(args);
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(arguments.storagePath);
+        config.setMaxDepthOfCrawling(1);
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+        controller.addSeed("https://cookpad.com/category/1232");
+
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        controller.start(CookpadCrawler.class, arguments.numberOfCrawlers);
+    }
+
+    private static Arguments parseArgs(final String[] args) {
+        final Arguments arguments = new Arguments();
+        final CmdLineParser parser = new CmdLineParser(arguments);
+        try {
+            parser.parseArgument(args);
+        } catch (final CmdLineException e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+        return arguments;
+    }
+
+    static class Arguments {
+        @Option(name = "--numberOfCrawlers")
+        private int numberOfCrawlers = 1;
+
+        @Option(name = "--storagePath")
+        private String storagePath;
+    }
 }
